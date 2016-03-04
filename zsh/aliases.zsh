843a3ea94586df5d6aa1318ef3d868f259aeeb95
Javier González <javier@javigon.com>
2016-03-05 00:12:01 +0100

    egrep: do not use deprecated flags

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -158,3 +158,5 @@ alias srdmt='spring rake db:migrate db:test:prepare'
 alias sp='sprintly'
 # spb = sprintly branch - create a branch automatically based on the bug you're working on
 alias spb="git checkout -b \`sp | tail -2 | grep '#' | sed 's/^ //' | sed 's/[^A-Za-z0-9 ]//g' | sed 's/ /-/g' | cut -d"-" -f1,2,3,4,5\`"
+
+alias egrep='egrep --color'
