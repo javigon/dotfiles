d19b7f8330705fafbfab0e15a4bc063a3cf85e45
Javier González <javier@javigon.com>
2019-11-06 19:48:22 +0100

    aliases: point to python3 by default
    Signed-off-by: Javier González <javier@javigon.com>

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -160,3 +160,5 @@ alias sp='sprintly'
 alias spb="git checkout -b \`sp | tail -2 | grep '#' | sed 's/^ //' | sed 's/[^A-Za-z0-9 ]//g' | sed 's/ /-/g' | cut -d"-" -f1,2,3,4,5\`"
 
 alias egrep='egrep --color'
+
+alias python=python3
