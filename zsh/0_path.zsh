0367d07f7f17c439267ff514f12df506256a8b73
Javier González <javier.gonz@samsung.com>
2019-11-20 07:31:08 +0100

    PATH: add .local/bin to path
    Signed-off-by: Javier González <javier.gonz@samsung.com>

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,4 +1,5 @@
 # path, the 0 in the filename causes this to load first
 export PATH=$PATH:$HOME/.yadr/bin:$HOME/.yadr/bin/yadr
 export PATH=$PATH:$HOME/utils
+export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:/Library/Tex/texbin
